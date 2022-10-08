990903916ef40966bfa675791fc12c2ff767e323
kohlbauers190178 <kohlbauers190178@sus.htl-grieskirchen.at>
2022-10-08 18:02:14 +0200

    TicTacToe with fragment

--- TIcTacToe/app/src/main/java/net/htlgkr/kohlbauers190178/tictactoe/MainActivity.java
+++ TIcTacToe/app/src/main/java/net/htlgkr/kohlbauers190178/tictactoe/MainActivity.java
@@ -9,89 +9,35 @@ import android.widget.TableRow;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    TicTacToeGame ticTacToeGame = new TicTacToeGame();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnStart = findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(this);
     }
 
-    public void btnClicked(View view) {
+    @Override
+    public void onClick(View view) {
         Button btnClicked = findViewById(view.getId());
-
         if (btnClicked.getId() == R.id.btnStart) {
+            btnClicked.setVisibility(View.GONE);
             startGame();
-        } else if (btnClicked.getId() == R.id.btnRestart) {
-            restartGame();
-        } else {
-            try {
-
-                String realIDFromButton = btnClicked.getResources().getResourceEntryName(view.getId());
-
-
-
-                //if (ticTacToeGame.isValidTurn(realIDFromButton)) {
-                if (ticTacToeGame.isValidTurn(btnClicked.getId())) {
-
-                    String infoForButton = ticTacToeGame.turnMade(btnClicked.getId());
-                    btnClicked.setText(infoForButton);
-
-                    if (ticTacToeGame.checkIfEnd()) {
-                        String causeOfEnd = ticTacToeGame.getCauseOfEnd();
-                        TextView txtView = findViewById(R.id.txtVwEndInfo);
-                        txtView.setText(causeOfEnd);
-
-                        endGame();
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
         }
     }
 
-    private void restartGame() {
-        TextView txtView = findViewById(R.id.txtVwEndInfo);
-        txtView.setText("");
-        endGame();
-        ticTacToeGame = new TicTacToeGame();
-        startGame();
-    }
 
-
-    private void endGame() {
-        ticTacToeGame.endGame();
-        LinearLayout layout = findViewById(R.id.linearLayoutVertical);
-        for (int i = 0; i < layout.getChildCount(); i++) {
-            LinearLayout horizontal = (LinearLayout) layout.getChildAt(i);
-            for (int j = 0; j < horizontal.getChildCount(); j++) {
-                Button button = (Button) horizontal.getChildAt(j);
-                button.setEnabled(false);
-            }
-        }
-    }
 
     public void startGame() {
-        Button startButton = findViewById(R.id.btnStart);
-        startButton.setVisibility(View.INVISIBLE);
-
-        LinearLayout layout = findViewById(R.id.linearLayoutVertical);
-        layout.setVisibility(View.VISIBLE);
-
-        for (int i = 0; i < layout.getChildCount(); i++) {
-            LinearLayout horizontal = (LinearLayout) layout.getChildAt(i);
-            for (int j = 0; j < horizontal.getChildCount(); j++) {
-                Button button = (Button) horizontal.getChildAt(j);
-                button.setText("");
-                button.setEnabled(true);
-            }
-        }
-        Button restartButton = findViewById(R.id.btnRestart);
-        restartButton.setVisibility(View.VISIBLE);
-        ticTacToeGame.startGame();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.constraintLayout, TicTacToeFragment.newInstance("",""), "TicTacToeFragment")
+                .commit();
     }
 }
--- TIcTacToe/app/src/main/java/net/htlgkr/kohlbauers190178/tictactoe/TicTacToeFragment.java
+++ TIcTacToe/app/src/main/java/net/htlgkr/kohlbauers190178/tictactoe/TicTacToeFragment.java
@@ -3,17 +3,24 @@ package net.htlgkr.kohlbauers190178.tictactoe;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import net.htlgkr.kohlbauers190178.tictactoe.databinding.ActivityMainBinding;
+import net.htlgkr.kohlbauers190178.tictactoe.databinding.FragmentTicTacToeBinding;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link TicTacToeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class TicTacToeFragment extends Fragment {
+public class TicTacToeFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,6 +34,9 @@ public class TicTacToeFragment extends Fragment {
     public TicTacToeFragment() {
         // Required empty public constructor
     }
+
+    TicTacToeGame ticTacToeGame;
+
 
     /**
      * Use this factory method to create a new instance of
@@ -53,12 +63,142 @@ public class TicTacToeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
+
+    FragmentTicTacToeBinding binding;
+
+    Button btnTL;
+    Button btnTM;
+    Button btnTR;
+
+    Button btnML;
+    Button btnMM;
+    Button btnMR;
+
+    Button btnBL;
+    Button btnBM;
+    Button btnBR;
+
+    Button btnReStart;
+
+    TextView txtViewEndInfo;
+
+    LinearLayout linearLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tic_tac_toe, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_tic_tac_toe,container,false);
+
+        linearLayout = view.findViewById(R.id.linearLayoutVertical);
+
+        txtViewEndInfo = view.findViewById(R.id.txtVwEndInfo);
+
+        btnTL = view.findViewById(R.id.btnTL);
+        btnTM = view.findViewById(R.id.btnTM);
+        btnTR = view.findViewById(R.id.btnTR);
+
+        btnML = view.findViewById(R.id.btnML);
+        btnMM = view.findViewById(R.id.btnMM);
+        btnMR = view.findViewById(R.id.btnMR);
+
+        btnBL = view.findViewById(R.id.btnBL);
+        btnBM = view.findViewById(R.id.btnBM);
+        btnBR = view.findViewById(R.id.btnBR);
+
+
+        btnReStart = view.findViewById(R.id.btnRestart);
+        btnReStart.setOnClickListener(this);
+
+
+        btnTL.setOnClickListener(this);
+        btnTM.setOnClickListener(this);
+        btnTR.setOnClickListener(this);
+
+        btnML.setOnClickListener(this);
+        btnMM.setOnClickListener(this);
+        btnMR.setOnClickListener(this);
+
+        btnBL.setOnClickListener(this);
+        btnBM.setOnClickListener(this);
+        btnBR.setOnClickListener(this);
+        //return inflater.inflate(R.layout.fragment_tic_tac_toe, container, false);
+        startGame();
+
+        return view;
     }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view.getId() == btnReStart.getId()) {
+            restartGame();
+        } else {
+            try {
+
+                String realIDFromButton = view.getResources().getResourceEntryName(view.getId());
+
+                //if (ticTacToeGame.isValidTurn(realIDFromButton)) {
+                if (ticTacToeGame.isValidTurn(view.getId())) {
+
+                    Button btn = (Button)view;
+
+                    String infoForButton = ticTacToeGame.turnMade(btn.getId());
+                    btn.setText(infoForButton);
+
+                    if (ticTacToeGame.checkIfEnd()) {
+                        String causeOfEnd = ticTacToeGame.getCauseOfEnd();
+                        txtViewEndInfo.setText(causeOfEnd);
+                        endGame();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void restartGame() {
+        txtViewEndInfo.setText("");
+        endGame();
+        ticTacToeGame = new TicTacToeGame();
+        startGame();
+    }
+
+
+    private void endGame() {
+        ticTacToeGame.endGame();
+        for (int i = 0; i < linearLayout.getChildCount(); i++) {
+            LinearLayout horizontal = (LinearLayout) linearLayout.getChildAt(i);
+            for (int j = 0; j < horizontal.getChildCount(); j++) {
+                Button button = (Button) horizontal.getChildAt(j);
+                button.setEnabled(false);
+            }
+        }
+    }
+
+    public void startGame() {
+
+        for (int i = 0; i < linearLayout.getChildCount(); i++) {
+            LinearLayout horizontal = (LinearLayout) linearLayout.getChildAt(i);
+            for (int j = 0; j < horizontal.getChildCount(); j++) {
+                Button button = (Button) horizontal.getChildAt(j);
+                button.setText("");
+                button.setEnabled(true);
+            }
+        }
+        
+        ticTacToeGame = new TicTacToeGame();
+        ticTacToeGame.startGame();
+    }
+
+
 }
